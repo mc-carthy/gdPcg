0361cb8731ce6d198d5043f6edc23aa6dc82fb06
Mick McCarthy <mick@mccarthy.tech>
2020-08-01 15:21:33 +0100

    Add path generation functions

--- SpelunkyLike/RandomWalker.gd
+++ SpelunkyLike/RandomWalker.gd
@@ -49,19 +49,74 @@ func _grid_to_world(grid_size) -> Vector2:
 	return Vector2()
 
 func _reset() -> void:
-	pass
+	_state = {
+		"random_index": -1,
+		"offset": Vector2.ZERO,
+		"delta": Vector2.ZERO,
+		"down_counter": 0,
+		"path": [],
+		"empty_cells": {}
+	}
+	for x in range(grid_size.x):
+		for y in range(grid_size.y):
+			_state.empty_cells[Vector2(x, y)] = 0
 
 func _update_start_position() -> void:
-	pass
+	var x: int = _rng.randi_range(0, grid_size.x - 1)
+	_state.offset = Vector2(x, 0)
 
 func _update_room_type() -> void:
-	pass
+	if not _state.path.empty():
+		var last: Dictionary = _state.path.back()
+	
+		if last.type in _rooms.BOTTOM_CLOSED and _state.delta.is_equal_approx(Vector2.DOWN):
+			var index := _rng.randi_range(0, _rooms.BOTTOM_OPENED.size() - 1)
+			var type: int = (
+				_rooms.BOTTOM_OPENED[index]
+				if _state.down_counter < 2
+				else _rooms.Type.LRTB
+			)
+			_state.path[-1].type = type
+	
+	var type: int = (
+		_rooms.Type.LRT
+		if _state.delta.is_equal_approx(Vector2.DOWN)
+		else _rng.randi_range(1, _rooms.Type.size() - 1)
+	)
+	
+	_state.empty_cells.erase(_state.offset)
+	_state.path.push_back({"offset": _state.offset, "type": type})
 
 func _update_next_position() -> void:
-	pass
+	_state.random_index = (
+		_rng.randi_range(0, STEP.size() - 1)
+		if _state.random_index < 0
+		else _state.random_index
+	)
+	_state.delta = STEP[_state.random_index]
+	
+	var horizontal_chance := _rng.randf()
+	if _state.delta.is_equal_approx(Vector2.LEFT):
+		_state.random_index = 0 if _state.offset.x > 1 and horizontal_chance < _horizontal_chance else 4
+	elif _state.delta.is_equal_approx(Vector2.RIGHT):
+		_state.random_index = 2 if _state.offset.x < grid_size.x - 1 and horizontal_chance < _horizontal_chance else 4
+	else:
+		if _state.offset.x > 0 and _state.offset.x < grid_size.x - 1:
+			_state.random_index = _rng.randi_range(0, 4)
+		elif _state.offset.x == 0:
+			_state.random_index = 2 if horizontal_chance < _horizontal_chance else 4
+		elif _state.offset.x == grid_size.x - 1:
+			_state.random_index = 0 if horizontal_chance < _horizontal_chance else 4
+	
+	_state.delta = STEP[_state.random_index]
+	_state.offset += _state.delta
 
 func _update_down_counter() -> void:
-	pass
+	_state.down_counter = (
+			_state.down_counter + 1
+			if _state.delta.is_equal_approx(Vector2.DOWN)
+			else 0
+		)
 
 func _place_walls() -> void:
 	pass
